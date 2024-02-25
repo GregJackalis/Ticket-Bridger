bf923d78d27160f98d20c3c4377af51d199710a0
TheJackal03 <gregorytsak3@gmail.com>
2024-02-25 21:00:59 +0200

    Added if statements to check when its time to handle a post request from a register form and when is it time to check a post request from a login form. Then, I added the login part which for now is very simple since no database is used. All it does is that it gets the given email and password, it trims any spaces etc, and then it checks if the given credentials are in the users object (And on the same user of course). After all these checks are done, the appropriate changes are made on the JSON response variable, and it is being sent at last at the front-end side on the json_handler script.

--- Middle End/main.php
+++ Middle End/main.php
@@ -3,13 +3,14 @@
     ini_set('display_errors', 1);
 
     include 'functions.php';
+    include 'users.php';
 
-    $username = $email = $password = "";
+    $username = $email = $password = $action = "";
     $usernameCheck = $emailCheck = $passwordCheck = false;
 
     $response = array(
         "status" => "error",
-        "message" => "Validation failed",
+        "message" => null,
         "errors" => array(
             "email" => null,
             "username" => null,
@@ -18,62 +19,77 @@
     );
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // in case the request method is post then it means that a user is trying to sign up most
-        //  defintely (for now at least...)
-        $username = setup_data($_POST["username_R"]);
-        $email = setup_data($_POST["email_R"]);
-        $password = $_POST["password_R"]; // the password's data doesn't need to be in the setup_data because it might affect the
-                                          // actual value of the it.
+        $action = $_POST["action"];
+        
+        if ($action == "signUpAction") {
+            // in case the request method is post then it means that a user is trying to sign up most
+            //  defintely (for now at least...)
+            $username = setup_data($_POST["username_R"]);
+            $email = setup_data($_POST["email_R"]);
+            $password = $_POST["password_R"]; // the password's data doesn't need to be in the setup_data because it might affect the
+                                            // actual value of the it.
 
-        $emailCheck = checkingEmail($email);
-        $usernameCheck = checkingUsername($username);
-        $passwordCheck = checkingPassword($password);
+            $emailCheck = checkingEmail($email);
+            $usernameCheck = checkingUsername($username);
+            $passwordCheck = checkingPassword($password);
 
-        if ($emailCheck && $usernameCheck && $passwordCheck) {
-            //  1st) encrypt the password with one or more different encryption algorithms (with the usage of secret keys 
-            //       stored in a .env file), and then put it in the variable that the username and password are already in.
+            if ($emailCheck && $usernameCheck && $passwordCheck) {
+                //  1st) encrypt the password with one or more different encryption algorithms (with the usage of secret keys 
+                //       stored in a .env file), and then put it in the variable that the username and password are already in.
 
-            //  2nd) insert email, username, and password to a variable that will be 
-            //  inserted to the database (if all the checks for each field are true)
-            //  need to use the "INSERT..." query to insert all email, username, and password
+                //  2nd) insert email, username, and password to a variable that will be 
+                //  inserted to the database (if all the checks for each field are true)
+                //  need to use the "INSERT..." query to insert all email, username, and password
+                $users[] = array(
+                    'email' => $email,
+                    'username' => $username,
+                    'password' => $password
+                );
+                
+                $response = array(
+                    "status" => "success",
+                    "message" => "Credentials have been Validated",
+                );
 
-            $response = array(
-                "status" => "success",
-                "message" => "Credentials have been Validated",
-            );
+            } else {
+                $response["message"] = "Validation failed";
+                if (!$emailCheck) {
+                    $response["errors"]["email"] = "Invalid Email Given";
+                }
 
-        } else {
-            if (!$emailCheck) {
-                $response['errors']['email'] = 'Invalid Email Given';
+                if (!$usernameCheck) {
+                    $response["errors"]["username"] = "Invalid Username Given";
+                }
+
+                if (!$passwordCheck) {
+                    $response["errors"]["password"] = "Invalid Password Given";
+                }
             }
 
-            if (!$usernameCheck) {
-                $response['errors']['username'] = 'Invalid Username Given';
+        } elseif ($action == "loginAction") {
+            $email = setup_data($_POST["email_L"]);
+            $password = setup_data($_POST["password_L"]);
+            if (empty($users)) {
+                $response["message"] = "There are no users registered yet!";
+            } else {
+                foreach ($users as $user) {
+                    if ($user['email'] == $email && $user['password'] == $password) {
+                        $response["status"] = "success";
+                        $response["message"] = "User has Successfully Logged In!";
+                        break;
+                    }
+                }
+                
+                // if status in the json response array/object has remained "error", then the credentials given are not correct
+                if ($response["status"] == "error") {
+                    $response["message"] = "User has given wrong credentials!";
+                }
             }
-
-            if (!$passwordCheck) {
-                $response['errors']['password'] = 'Invalid Password Given';
-            }
+            // $response["message"] = $users;
         }
-
-        // header('Content-Type: application/json');
-
-        // file_put_contents('main_php_debug.txt', json_encode($response));
-
-        // Output the JSON response containing information about which credential was invalid
-        // echo json_encode($response);
-
-        $html_response = "<h2>Errors while Signing up!</h2><br>";
-        $errors = $response['errors'];
-
-        foreach ($errors as $error) {
-            if ($error !== null) {
-                $html_response .= "<p>$error</p><br>";
-            }
-        }
-
-        echo $html_response;
-
+        
+        header('Content-Type: application/json');
+        echo json_encode($response);
 
     }
 ?>
