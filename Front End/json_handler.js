100abaef249561a61ed4d74864190c41a17126f4
TheJackal03 <gregorytsak3@gmail.com>
2024-02-28 00:07:27 +0200

    Commented out the code that is about printing each error returned from the back end. I also add the wrapper class to the phpElement as soon as I access it and then I also add the appropriate message or error-message class to the phpElement. Lastly, I add each occasion the active and active-group classes to the phpElement so that the message based on the back-end's info is displayed on the user's screen. Also at the end, I've added a few more classes for the icon-close class/element to remove when it's pressed.

--- Front End/json_handler.js
+++ Front End/json_handler.js
@@ -1,12 +1,15 @@
 var phpElement = document.getElementById('phpResponse');
+phpElement.classList.add('wrapper');
 var newEl = document.createElement('div');
+
+var errMessage = false; // this is used in order to check whether the message is for an error, 
+// so that the appropriate class will be used
 
 // FOR SIGN UP
 $(function() {
     $("#submitBtn").click(function(event) {
         console.log("register button pressed!!")
 
-        phpElement.innerHTML = ""; // Clear previous content
         newEl.innerHTML = ""; // Clear previous content
         event.preventDefault();
 
@@ -19,21 +22,28 @@ $(function() {
             console.log(response); // Log response for debugging
 
             if (response.status == 'success') {
-                newEl.innerHTML = response.message;
+                newEl.innerHTML = '<p class="popup-font">Registered Successfully!</p>';
+                phpElement.classList.add('message');
             } else {
-                var header = document.createElement('h2');
-                header.innerHTML = "Errors while Signing up:<br>";
-                phpElement.appendChild(header);
-                newEl.innerHTML = "<p>" + response.message + "</p><br>";
-                for (var key in response.errors) {
-                    if (response.errors.hasOwnProperty(key) && response.errors[key] !== null) {
-                        newEl.innerHTML += "<p>" + response.errors[key] + "</p><br>";
-                    }
-                }
+                // var header = document.createElement('h2');
+                // header.innerHTML = "Errors while Signing up:<br>";
+                // phpElement.appendChild(header);
+                // newEl.innerHTML = "<p>" + response.message + "</p><br>";
+                // for (var key in response.errors) {
+                //     if (response.errors.hasOwnProperty(key) && response.errors[key] !== null) {
+                //         newEl.innerHTML += "<p>" + response.errors[key] + "</p><br>";
+                //     }
+                // }
+                newEl.innerHTML = '<p class="popup-font">Email must be of valid type!<br>';
+                newEl.innerHTML += 'Password must contain at least a number, an uppercase character,'
+                newEl.innerHTML += ' a lowercase character, a special character, and its length <br>to be more than 8 characters.</p>';
+                phpElement.classList.add('error-message');
+                errMessage = true;
             }
-
             phpElement.appendChild(newEl);
         });
+        phpElement.classList.add('active');
+        phpElement.classList.add('active-popup');
     });
 });
 
@@ -42,8 +52,6 @@ $(function() {
 // FOR LOGIN
 $(function() {
     $("#loginBtn").click(function(event) {
-        console.log("login button pressed!!")
-        phpElement.innerHTML = ""; // Clear previous content
         newEl.innerHTML = ""; // Clear previous content
         event.preventDefault();
 
@@ -56,20 +64,38 @@ $(function() {
             console.log(response); // Log response for debugging
 
             if (response.status == 'success') {
-                newEl.innerHTML = response.message;
+                newEl.innerHTML = '<p class="popup-font">Logged In Successfully!</p>';
+                phpElement.classList.add('message');
             } else {
-                var header = document.createElement('h2');
-                header.innerHTML = "Errors while Logging in:<br>"; // Changed header text
-                phpElement.appendChild(header);
-                newEl.innerHTML = "<p>" + response.message + "</p><br>";
-                for (var key in response.errors) {
-                    if (response.errors.hasOwnProperty(key) && response.errors[key] !== null) {
-                        newEl.innerHTML += "<p>" + response.errors[key] + "</p><br>";
-                    }
-                }
+                // var header = document.createElement('h2');
+                // header.innerHTML = "Errors while Logging in:<br>"; // Changed header text
+                // phpElement.appendChild(header);
+                // newEl.innerHTML = "<p>" + response.message + "</p><br>";
+                // for (var key in response.errors) {
+                //     if (response.errors.hasOwnProperty(key) && response.errors[key] !== null) {
+                //         newEl.innerHTML += "<p>" + response.errors[key] + "</p><br>";
+                //     }
+                // }
+
+                newEl.innerHTML = '<p class="popup-font">Incorrect Credentials!</p>';
+                phpElement.classList.add('message');
             }
 
             phpElement.appendChild(newEl);
         });
+        phpElement.classList.add('active');
+        phpElement.classList.add('active-popup');
     });
 });
+
+$('.icon-close').click(()=> {
+    phpElement.classList.remove('active');
+    phpElement.classList.remove('active-popup');
+    if (errMessage == true) {
+        errMessage = false;
+        phpElement.classList.remove('error-message');
+    } else {
+        phpElement.classList.remove('message');
+    }
+  });
+  
